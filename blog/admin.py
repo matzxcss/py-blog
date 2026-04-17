f2f672a41ac43f593e62dd61536412dce0eec246
matzxcss <matheuslacerda01@proton.me>
2026-04-17 20:22:36 -0300

    fix any errors

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
 from .models import Commentary, Post, User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class CustomUserAdmin(BaseUserAdmin):
     pass
 
 
